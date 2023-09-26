2454c5c2f2258e0aab4666688e622e216854a6c5
joaogabrieldeassis <joaoassisgabriel@gmail.com>
2023-09-26 14:57:10 -0300

    Criando o back-end

---
+++ Services/SmartScoolApi/SmartScoolApi/Controllers/AlunoController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SmartScoolApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AlunoController : ControllerBase
+    {
+
+    }
+}
